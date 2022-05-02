cf16286f1806c9f6473902aedae97239e371c853
Ashish Mishra <anshm9448@gmail.com>
2022-05-02 12:57:00 +0530

    Intial Commit

---
+++ lib/constants/app_constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#1c1c1c");
+final Color accentHexColor = HexColor("#FCC91C");
+
